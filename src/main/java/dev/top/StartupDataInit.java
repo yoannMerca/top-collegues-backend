7108d06acc981cab1ff1a7083fa011cd20124f27
Rossi Oddet <rossi.oddet@dev-institut.fr>
2019-02-20 09:30:02 +0100

    drop and create

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -1,11 +1,12 @@
 package dev.top;
 
-import dev.top.entities.Version;
-import dev.top.repos.VersionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import dev.top.entities.Version;
+import dev.top.repos.VersionRepo;
 
 @Component
 public class StartupDataInit {
@@ -16,12 +17,10 @@ public class StartupDataInit {
     @EventListener(ContextRefreshedEvent.class)
     public void init() {
 
-        if(this.versionRepo.count() <= 0) {
-            this.versionRepo.save(new Version("v1"));
-            this.versionRepo.save(new Version("v2"));
-            this.versionRepo.save(new Version("v3"));
-            this.versionRepo.save(new Version("v4"));
-        }
+        this.versionRepo.save(new Version("v1"));
+        this.versionRepo.save(new Version("v2"));
+        this.versionRepo.save(new Version("v3"));
+        this.versionRepo.save(new Version("v4"));
 
     }
 }
