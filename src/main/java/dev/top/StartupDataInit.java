809d5ba74a2eb65f3395d0531a0e4579cf98868e
mercadier yoann <yoann.mercadier@gmail.com>
2019-03-28 09:49:19 +0100

    add other collegues

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -27,5 +27,10 @@ public class StartupDataInit {
         this.versionRepo.save(new Version("v3"));
         this.versionRepo.save(new Version("v4 dffbsdfghs"));
         this.collegueRepo.save(new Collegue("yoyo","https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/One_Direction_NRJ_2014_3.jpg/330px-One_Direction_NRJ_2014_3.jpg", 100));
+        this.collegueRepo.save(new Collegue("youyou","https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Harry_Styles_November_2014.jpg/330px-Harry_Styles_November_2014.jpg", 150));
+        this.collegueRepo.save(new Collegue("yoyo2","https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/One_Direction_NRJ_2014_3.jpg/330px-One_Direction_NRJ_2014_3.jpg",110));
+        this.collegueRepo.save(new Collegue("youyou2","https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Harry_Styles_November_2014.jpg/330px-Harry_Styles_November_2014.jpg",150));
+        this.collegueRepo.save(new Collegue("yoyo3","https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/One_Direction_NRJ_2014_3.jpg/330px-One_Direction_NRJ_2014_3.jpg", 100));
+        this.collegueRepo.save(new Collegue("youyou3","https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Harry_Styles_November_2014.jpg/330px-Harry_Styles_November_2014.jpg",150));
     }
 }
